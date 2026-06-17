fcfe38440854763bd47fe1b46de0594f70e40387
HaoTran <31514669+donxiya@users.noreply.github.com>
2026-06-17 00:15:48 -0400

    fix port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import weatherRoutes from "./routes/weather.routes";
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
 app.use("/weather", weatherRoutes);
 
-app.listen(3001, () => {
-  console.log("Backend running on port 3001");
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
