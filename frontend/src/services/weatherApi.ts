4596936078e7e5dc87174e7beff6fc17842b1dce
HaoTran <31514669+donxiya@users.noreply.github.com>
2026-06-17 00:21:39 -0400

    update frontend fetch url

--- frontend/src/services/weatherApi.ts
+++ frontend/src/services/weatherApi.ts
@@ -1,9 +1,9 @@
 import type { WeatherResponse } from "../types/weather";
 
+const API_URL = "https://weather-analytics-tls1.onrender.com";
+
 export async function getWeather(city: string): Promise<WeatherResponse> {
-  const res = await fetch(
-    `http://localhost:3001/weather?city=${city}`
-  );
+  const res = await fetch(`${API_URL}/weather?city=${city}`)
 
   if (!res.ok) {
     throw new Error("Failed to fetch weather");
