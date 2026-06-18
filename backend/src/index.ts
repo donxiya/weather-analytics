9facf79fc6e142a35004ae9b160f4e0749aecd9f
HaoTran <31514669+donxiya@users.noreply.github.com>
2026-06-17 22:40:37 -0400

    update read body

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,9 @@ import weatherRoutes from "./routes/weather.routes";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 app.use("/weather", weatherRoutes);
 
